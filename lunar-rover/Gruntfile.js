b2ea80ac0fd7c64b7e15865ee13adc2c95be0b59
Mark Brown <markbrown4@gmail.com>
2017-11-23 10:53:47 +0000

    add start script for grunt

--- lunar-rover/Gruntfile.js
+++ lunar-rover/Gruntfile.js
@@ -1,37 +1,35 @@
 module.exports = function (grunt) {
-
     // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        babel: {
-            options: {
-                sourceMap: false,
-                presets: ['es2015']
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: 'es6',
-                    src: ['*.js'],
-                    dest: 'js'
-                }]
-            }
-        },
-        watch: {
-            js: {
-                files: ['es6/**'],
-                tasks: ['babel'],
-                options: {
-                    spawn: false
-                }
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    babel: {
+      options: {
+        sourceMap: false,
+        presets: ['es2015']
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'es6',
+          src: ['*.js'],
+          dest: 'js'
+        }]
+      }
+    },
+    watch: {
+      js: {
+        files: ['es6/**'],
+        tasks: ['babel'],
+        options: {
+          spawn: false
         }
-    });
+      }
+    }
+  })
 
-    grunt.loadNpmTasks('grunt-babel');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-babel')
+  grunt.loadNpmTasks('grunt-contrib-watch')
 
     // Default task(s).
-    grunt.registerTask('default', ['babel', 'watch']);
-
-};
+  grunt.registerTask('default', ['babel', 'watch'])
+}
--- lunar-rover/es6/script.js
+++ lunar-rover/es6/script.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 // Model the Moon, Lander and Rover as separate objects
 
